0d7f1af10007f29ce355713194a8a3523c0cb0c7
Jose Alvarez <jp.alvarezl@gmail.com>
2025-07-04 10:43:39 +0200

    Chat behaviour correct

---
+++ tpm-agent/app/repo.tsx
@@ -0,0 +1,9 @@
+export default function Repo() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-2xl text-gray-600 dark:text-gray-400">
+        Under construction
+      </div>
+    </div>
+  );
+}
